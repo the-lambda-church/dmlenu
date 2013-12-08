98961c25652a59511015439a57ecdb6244550b07
Simon Castellan <simon.castellan@iuwt.fr>
2013-12-08 17:48:15 +0100

    Matching: implemented fuzzy matching.

--- lib/matching.ml
+++ lib/matching.ml
@@ -3,6 +3,17 @@ open Batteries
 
 type result = ((bool * int * int) list)
 
+
+let make_list candidate list = 
+  let list, old = 
+    List.fold_left (fun (list, old) (start', stop') ->
+      (true, start', stop') :: (false, old, start') :: list, stop'
+    ) ([], 0) list
+  in
+  let list = List.filter (fun (_, k, k') -> k <> k') 
+    (List.rev ((false, old, String.length candidate) :: list)) in
+  list
+
 let match_query ~case ~query ~candidate = 
   let query, candidate = 
     if case then 
@@ -11,11 +22,15 @@ let match_query ~case ~query ~candidate =
       String.lowercase query, String.lowercase candidate
   in
   try
-    let k = String.find candidate query in
-    Some [
-      (false, 0, k) ;
-      (true, k, k + String.length query) ;
-      (false, k + String.length query, String.length candidate) ;
-    ]
-  with _ -> None
+    let words = List.filter ((<>) "") (String.nsplit query " ") in
+    let matches = List.map (fun word ->
+      let n = String.find candidate word in
+      (n, n + String.length word)) words
+    in 
+    Some 
+      (make_list candidate 
+         (List.sort (fun x y -> compare (fst x) (fst y)) matches))
+  with Not_found -> None
   
+
+    
