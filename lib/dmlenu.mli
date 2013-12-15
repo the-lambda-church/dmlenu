5922bef0229e59523a4aa6807631213d57e2af32
Thomas Refis <refis.thomas@gmail.com>
2013-12-15 16:38:59 +0100

    update doc for [Dmlenu.run]

--- lib/dmlenu.mli
+++ lib/dmlenu.mli
@@ -30,7 +30,20 @@ val run : ?source_transition_hook:(Completion.state -> conf -> conf) -> app_stat
     Returns [Some user_input] when the user valid a completion (i.e. hits RET)
     or [None] when the user cancels (i.e. hits ESC)
 
-    N.B. this function is blocking. *)
+    N.B. this function is blocking.
+
+    The [source_transition_hook] parameter is to be used when you want to
+    perform side effects, or update the configuration when a transition happens.
+    The reason why you would want use this hook insteal of directly performing
+    your computation in the transition function of the state machine (see
+    {!Completion}) is:
+    - You cannot modify the configuration in the machine transition function
+      (although we could allow it).
+    - This function will be called when the user completes something, but also
+      when he removes some of its input : i.e. when we backtrack in the
+      state_machine/automata, whereas the transition function is called only
+      when the user completes.
+*)
 
 (* TODO: move before [run], more explicit documentation *)
 val run_list : ?source_transition_hook:(Completion.state -> conf -> conf) -> app_state -> conf -> string list
